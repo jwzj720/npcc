348efb2e6bda76e14f61af9ab157688b02fcc41d
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-15 14:07:53 -0800

    added longer runtime

--- mainloop.cu
+++ mainloop.cu
@@ -481,7 +481,7 @@ int main() {
     initializePond<<<POND_SIZE_X, POND_SIZE_Y>>>(d_pond);
 
    // Call the kernel function
-    for (uint64_t n = 0; n < 1; n++){
+    for (uint64_t n = 0; n < 1000; n++){
         run<<<1, 1>>>(d_pond, d_buffer, d_in, d_prngState, d_statCounters, cellIdCounter);
         cudaDeviceSynchronize(); 
         cudaMemcpy(statCounters, d_statCounters, sizeof(struct statCounters), cudaMemcpyDeviceToHost);  
