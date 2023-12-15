fea4641e7b722f3c1b5b556952d80a31cc52a357
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-15 15:00:33 -0800

    added test

---
+++ test.cu
@@ -0,0 +1,16 @@
+#include<cuda.h>
+
+__device__ void haha(uintptr_t *ret)
+{
+    ret = 0;
+}
+__global__ void run()
+{
+    uintptr_t ret;
+    haha(&ret);
+    printf("ret = %d\n", (void*)ret);
+}
+main(){
+    run<<<1,1>>>();
+    cudaDeviceSynchronize();
+}
